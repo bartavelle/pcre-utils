68f5665be3de66aef964f146f4b6d644c820f15f
bartavelle <bartavelle@gmail.com>
2014-03-04 10:49:59 +0100

    Pure version of the functions

--- Text/Regex/PCRE/ByteString/Utils.hs
+++ Text/Regex/PCRE/ByteString/Utils.hs
@@ -7,10 +7,24 @@ Right "l'a'pin"
 Right ["a","b","c"]
 -}
 module Text.Regex.PCRE.ByteString.Utils
-    ( substitute
+    ( -- * Perl-like utility functions
+      substitute
     , split
     , substituteCompile
     , splitCompile
+    -- * Re-exports from "Text.Regex.PCRE.ByteString"
+    , Regex
+    , CompOption
+    , ExecOption
+    , compBlank
+    , execBlank
+    -- * Pure version of the functions, using 'unsafePerformIO'
+    , compile'
+    , execute'
+    , substitute'
+    , split'
+    , substituteCompile'
+    , splitCompile'
     ) where
 
 import Text.Regex.PCRE.ByteString
@@ -20,6 +34,8 @@ import qualified Data.Vector as V
 import Data.Attoparsec.ByteString.Char8 as A
 import Control.Applicative
 import Data.Char (digitToInt)
+import System.IO.Unsafe
+import qualified Data.Array as A
 
 {-| Substitutes values matched by a `Regex`. References can be used.
 
@@ -59,6 +75,19 @@ split :: Regex  -- ^ The regular expression, taken from a call to `compile`
 split regexp srcstring = either Left (Right . removeEmptyLeft . regexpUnmatched . fst) <$> runErrorT (getMatches regexp srcstring V.empty)
     where
         removeEmptyLeft = reverse . dropWhile BS.null . reverse
+
+-- | A pure version of 'substitute', using unsafePerformIO.
+substitute' :: Regex             -- ^ The regular expression, taken from a call to `compile`
+            -> BS.ByteString     -- ^ The source string
+            -> BS.ByteString     -- ^ The replacement string
+            -> Either String BS.ByteString
+substitute' regexp srcstring repla = unsafePerformIO (substitute regexp srcstring repla)
+
+-- | A pure version of 'split', using unsafePerformIO.
+split' :: Regex  -- ^ The regular expression, taken from a call to `compile`
+       -> BS.ByteString -- ^ The source string
+       -> Either String [BS.ByteString]
+split' regexp srcstring = unsafePerformIO (split regexp srcstring)
 
 data RegexpSplit a = Matched a
                    | Unmatched a
@@ -139,3 +168,24 @@ splitCompile regexp srcstring = do
     case re of
         Right cre -> split cre srcstring
         Left rr   -> return $ Left $ "Regexp compilation failed: " ++ show rr
+
+-- | A pure version of 'compile', using unsafePerformIO.
+compile' :: CompOption -> ExecOption -> BS.ByteString -> Either (MatchOffset, String) Regex
+compile' co eo s = unsafePerformIO (compile co eo s)
+
+-- | A pure version of 'execute', using unsafePerformIO.
+execute' :: Regex -> BS.ByteString -> Either WrapError (Maybe (A.Array Int (MatchOffset, MatchLength)))
+execute' r s = unsafePerformIO (execute r s)
+
+-- | A pure version of 'substituteCompile', using unsafePerformIO.
+substituteCompile' :: BS.ByteString     -- ^ The regular expression
+                   -> BS.ByteString     -- ^ The source string
+                   -> BS.ByteString     -- ^ The replacement string
+                   -> Either String BS.ByteString
+substituteCompile' regexp srcstring repla = unsafePerformIO (substituteCompile regexp srcstring repla)
+
+-- | A pure version of 'splitCompile', using unsafePerformIO.
+splitCompile' :: BS.ByteString -- ^ The regular expression
+              -> BS.ByteString -- ^ The source string
+              -> Either String [BS.ByteString]
+splitCompile' regexp srcstring = unsafePerformIO (splitCompile regexp srcstring)
